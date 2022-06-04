fe73b2b86182dfda92bc870ef1db557ed135b858
SumeetShelar <56186357+SumeetShelar@users.noreply.github.com>
2022-06-04 18:22:01 +0530

    Navbar section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
+import { Navbar, Footer, Loader, Services, Transactions, Welcome } from './components';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />  
+      </div>
+        {/* <Loader /> */}
+        <Services />
+        <Transactions />
+        <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
